342744661936135f7e3f7c95986e9148d1b7dbc6
SUPPASAK MATAWANTHANAKORN <suppasak.mata@bumail.net>
2016-02-10 10:46:32 +0700

    Ending the game

--- space shooter/Assets/script/GameController.cs
+++ space shooter/Assets/script/GameController.cs
@@ -10,15 +10,38 @@ public class GameController : MonoBehaviour {
 	public float waveWait;
 
 	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private bool gameOver;
+	private bool restart;
 	private int score;
 
 	void Start ()
 	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
 		score = 0;
 		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
 
 	}
+
+	void Update()
+
+	{
+		if (restart) {
+
+			if(Input.GetKeyDown (KeyCode .R))
+			{
+				Application.LoadLevel(Application.loadedLevel); 
+			}
+		}
+	}
+
+
 	IEnumerator SpawnWaves ()
 	{
 		yield return new WaitForSeconds (startWait);
@@ -31,6 +54,13 @@ public class GameController : MonoBehaviour {
 				yield return new WaitForSeconds (spawnWait);
 			}
 			yield return new WaitForSeconds (waveWait);
+
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				break;
+			}
 		}
 	}
 
@@ -42,6 +72,12 @@ public class GameController : MonoBehaviour {
 	void UpdateScore ()
 	{
 		scoreText.text = "Score : " + score;
+	}
+
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
 	}
 
 	
