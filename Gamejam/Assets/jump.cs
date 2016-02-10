0bab16cd36dbd8417ff9cc7281beaff36d4f8d89
SUPPASAK MATAWANTHANAKORN <suppasak.mata@bumail.net>
2016-02-10 12:42:49 +0700

    Game jam 1

---
+++ Gamejam/Assets/jump.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jump : MonoBehaviour {
+	public float up;
+	private  Rigidbody rb;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		
+	}
+	
+	void FixedUpdate ()
+	{
+		if (Input.GetKey (KeyCode.A)) {
+			rb.AddForce (Vector3.up * up);
+		}
+	}
+}
